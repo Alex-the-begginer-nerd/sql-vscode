7f2cf0ea06ad8d1a78aa55d7e7a7f3cb842ec84d
Alex-the-begginer-nerd <kozachenko08@bk.ru>
2025-03-01 13:19:45 +0300

    https://github.com/Alex-the-begginer-nerd/sql-vscode/edit/main/README.md

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,12 @@
+select 
+    skills,
+    count(skills_job_dim.job_id) demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst' AND 
+    job_work_from_home = true
+GROUP BY skills
+ORDER BY demand_count DESC
+limit 5
